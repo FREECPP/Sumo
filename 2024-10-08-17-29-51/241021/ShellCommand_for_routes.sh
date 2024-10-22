ad3908a1085b716c817da885638805c15da980fb
FREECPP <felix.rehm2000@gmail.com>
2024-10-22 11:02:25 +0200

    worked on a solution to get all routefiles into one simulation

--- 2024-10-08-17-29-51/241021/ShellCommand_for_routes.sh
+++ 2024-10-08-17-29-51/241021/ShellCommand_for_routes.sh
@@ -1,1 +1,6 @@
+#For cars
 python "/opt/homebrew/opt/sumo/share/sumo/tools/randomTrips.py" -n "/opt/homebrew/opt/sumo/share/sumo/tools/2024-10-08-17-29-51/241018_A034.net.xml" -r 241021_random_route.rou.xml --vehicle-class passenger --seed 42 --begin 0 --end 3600  --insertion-density 40 --random-depart -v
+#For bicycles
+python "/opt/homebrew/opt/sumo/share/sumo/tools/randomTrips.py" -n "/opt/homebrew/opt/sumo/share/sumo/tools/2024-10-08-17-29-51/241018_A034.net.xml" -r 241022_random_route_bicycles.rou.xml --vehicle-class bicycle --seed 42 --begin 0 --end 3600  --insertion-density 70 --random-depart -v --prefix bike
+#For all vehicles
+python "/opt/homebrew/opt/sumo/share/sumo/tools/randomTrips.py" -n "/opt/homebrew/opt/sumo/share/sumo/tools/2024-10-08-17-29-51/241018_A034.net.xml" -r 241022_random_route.rou.xml --vehicle-class passenger --seed 42 --begin 0 --end 3600  --insertion-density 70 --random-depart -v --prefix passenger
