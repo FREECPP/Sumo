23d3120b639a071cffeaf9e30ea0cd9f10e2dd24
FREECPP <felix.rehm2000@gmail.com>
2024-10-31 13:38:08 +0100

    added busstops; tried to make a bus route-file that follows the stops(failed); had a look at the other junctions that were not mentioned(still work to do)

--- 2024-10-08-17-29-51/241021/ShellCommand_for_routes.sh
+++ 2024-10-08-17-29-51/241021/ShellCommand_for_routes.sh
@@ -1,6 +1,9 @@
 #For cars
-python "/opt/homebrew/opt/sumo/share/sumo/tools/randomTrips.py" -n "/opt/homebrew/opt/sumo/share/sumo/tools/2024-10-08-17-29-51/241026/241026_A035.net.xml" -r 241026_2_random_route_cars.rou.xml --vehicle-class passenger --seed 42 --begin 0 --end 3600  --insertion-density 40 --random-depart -v
+python "/opt/homebrew/opt/sumo/share/sumo/tools/randomTrips.py" -n "/opt/homebrew/opt/sumo/share/sumo/tools/2024-10-08-17-29-51/241031/241031_A131.net.xml" -r 241031_random_route_cars.rou.xml --vehicle-class passenger --seed 42 --begin 0 --end 3600  --insertion-density 40 --random-depart -v
 #For bicycles
-python "/opt/homebrew/opt/sumo/share/sumo/tools/randomTrips.py" -n "/opt/homebrew/opt/sumo/share/sumo/tools/2024-10-08-17-29-51/241026/241026_A035.net.xml" -r 241026_2_random_route_bicycles.rou.xml --vehicle-class bicycle --seed 42 --begin 0 --end 3600  --insertion-density 70 --random-depart -v --prefix bike
+python "/opt/homebrew/opt/sumo/share/sumo/tools/randomTrips.py" -n "/opt/homebrew/opt/sumo/share/sumo/tools/2024-10-08-17-29-51/241031/241031_A131.net.xml" -r 241031_random_route_bicycles.rou.xml --vehicle-class bicycle --seed 42 --begin 0 --end 3600  --insertion-density 70 --random-depart -v --prefix bike
 #For all vehicles
-python "/opt/homebrew/opt/sumo/share/sumo/tools/randomTrips.py" -n "/opt/homebrew/opt/sumo/share/sumo/tools/2024-10-08-17-29-51/241026/241026_A035.net.xml" -r 241026_2_random_route_pedestrian.rou.xml --pedestrians --seed 42 --begin 0 --end 3600  --insertion-density 70 --random-depart -v --prefix pedestrian
+python "/opt/homebrew/opt/sumo/share/sumo/tools/randomTrips.py" -n "/opt/homebrew/opt/sumo/share/sumo/tools/2024-10-08-17-29-51/241031/241031_A131.net.xml" -r 241031_random_route_pedestrian.rou.xml --pedestrians --seed 42 --begin 0 --end 3600  --insertion-density 70 --random-depart -v --prefix pedestrian
+#For busses
+python "/opt/homebrew/opt/sumo/share/sumo/tools/randomTrips.py" -n "/opt/homebrew/opt/sumo/share/sumo/tools/2024-10-08-17-29-51/241031/241031_A131.net.xml" -r 241031_random_route_bus.rou.xml --additional-files /opt/homebrew/opt/sumo/share/sumo/tools/2024-10-08-17-29-51/241031/bus_stops.add.xml --vehicle-class bus --seed 42 --begin 0 --end 3600  --insertion-density 40 --random-depart -v --prefix bus --validate
+
